5db8dfe0163ce15bec5452c464e64889aa9497ee
Ligao Zhang <zhangligao9509@gmail.com>
2020-04-15 15:46:24 +0800

    save

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="multi_person_tracker",
+    name="person_tracker",
     version="0.1",
     author="Muhammed Kocabas",
     description="Multi Person Tracker",
